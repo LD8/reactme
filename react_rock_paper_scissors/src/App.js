afd0dc2d076676750174f9b84f51fa9bfd9c0c8a
ld8 <don_lee@me.com>
2020-03-29 00:07:41 +0800

    fully functional

--- react_rock_paper_scissors/src/App.js
+++ react_rock_paper_scissors/src/App.js
@@ -1,91 +1,219 @@
 import React, { Component } from "react";
-import rock from "./img/rock.png"
-import paper from "./img/paper.png"
-import scissors from "./img/scissors.png"
-import "./index.css";
+import Rock_img from "./img/rock.png";
+import Paper_img from "./img/paper.png";
+import Scissors_img from "./img/scissors.png";
 
-function ScoreBoard() {
-  return (
-    <div id="scoreBoard">
-      <p className="badge-left">
-        <span>My Score</span> <span id="myScore">0</span>
-      </p>
-      <p className="badge-right">
-        <span id="computerScore">0</span> <span>Mac Score</span>
-      </p>
-    </div>
-  );
-}
+const weapons = [
+  { name: "Rock", img: Rock_img },
+  { name: "Paper", img: Paper_img },
+  { name: "Scissors", img: Scissors_img }
+];
 
-function Status() {
+function Info({ myChoice }) {
   return (
     <div>
-      <p id="status">Who wins?</p>
-      <p id="tellStatus"></p>
+      <p id="info">
+        {myChoice ? "My choice was " + myChoice : "Choose to start the game!"}
+      </p>
     </div>
   );
 }
 
-function SignBoxes() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myChoice: "",
+      computerChoice: "",
+      myScore: 0,
+      computerScore: 0,
+      winner: ""
+    };
+  }
+
+  game = choice => {
+    this.setState({ myChoice: choice });
+    let counter = 0;
+    const id = setInterval(() => {
+      counter++;
+      this.setComputerChoice();
+      if (counter > 4) {
+        clearInterval(id);
+
+        const winner = this.evaluate(
+          this.state.myChoice,
+          this.state.computerChoice
+        );
+        this.setState({
+          winner: winner
+        });
+      }
+    }, 100);
+  };
+
+  // game = choice => {
+  //   this.setState({ myChoice: choice });
+  //   this.setComputerChoice();
+
+  //   const winner = this.evaluate(
+  //     this.state.myChoice,
+  //     this.state.computerChoice
+  //   );
+  //   this.setState({
+  //     winner: winner
+  //   });
+  // };
+
+  // game = choice => {
+  //   this.setState({ myChoice: choice });
+  //   this.setComputerChoice();
+
+  //   setTimeout(
+  //     () => this.evaluate(this.state.myChoice, this.state.computerChoice),
+  //     5
+  //   );
+  // };
+
+  // evaluate = (myChoice, computerChoice) => {
+  //   if (myChoice === computerChoice) {
+  //     console.log(this.state);
+  //     this.setState({
+  //       winner: "DRAW!"
+  //     });
+  //     this.glow(myChoice, "draw-glow");
+  //   } else if (
+  //     (myChoice === "Rock" && computerChoice === "Scissors") ||
+  //     (myChoice === "Scissors" && computerChoice === "Paper") ||
+  //     (myChoice === "Paper" && computerChoice === "Rock")
+  //   ) {
+  //     this.setState({ myScore: this.state.myScore + 1 });
+  //     console.log(this.state);
+  //     this.setState({
+  //       winner: "I WON!!"
+  //     });
+  //     this.glow(myChoice, "win-glow");
+  //   } else {
+  //     this.setState({ computerScore: this.state.computerScore + 1 });
+  //     console.log(this.state);
+  //     this.setState({
+  //       winner: "I LOST..."
+  //     });
+  //     this.glow(myChoice, "lose-glow");
+  //   }
+  // };
+
+  setComputerChoice = () =>
+    this.setState({
+      computerChoice: weapons[Math.floor(Math.random() * weapons.length)].name
+    });
+
+  evaluate = (myChoice, computerChoice) => {
+    if (myChoice === computerChoice) {
+      console.log(this.state);
+      this.glow(myChoice, "draw-glow");
+      return "DRAW!";
+    } else if (
+      (myChoice === "Rock" && computerChoice === "Scissors") ||
+      (myChoice === "Scissors" && computerChoice === "Paper") ||
+      (myChoice === "Paper" && computerChoice === "Rock")
+    ) {
+      this.setState({ myScore: this.state.myScore + 1 });
+      console.log(this.state);
+      this.glow(myChoice, "win-glow");
+      return "I WIN!";
+    } else {
+      this.setState({ computerScore: this.state.computerScore + 1 });
+      console.log(this.state);
+      this.glow(myChoice, "lose-glow");
+      return "I LOST...";
+    }
+  };
+
+  glow = (myChoice, glow) => {
+    const elem = document.getElementById(myChoice);
+    elem.classList.add(glow);
+    setTimeout(() => elem.classList.remove(glow), 500);
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <div id="scoreBoard">
+            <p className="badge-left">
+              <span>My Score</span>{" "}
+              <span id="myScore">{this.state.myScore}</span>
+            </p>
+            <p className="badge-right">
+              <span id="computerScore">{this.state.computerScore}</span>{" "}
+              <span>Mac Score</span>
+            </p>
+          </div>
+
+          <div>
+            <p id="status">
+              {this.state.winner ? this.state.winner : "Who wins?"}
+            </p>
+            <p id="tellStatus">
+              {this.state.myChoice
+                ? this.state.myChoice + " VS " + this.state.computerChoice
+                : null}
+            </p>
+          </div>
+
+          <div className="sign-boxes">
+            {weapons.map(weapon => {
+              return (
+                <div
+                  key={weapon.name}
+                  id={weapon.name}
+                  onClick={() => this.game(weapon.name)}
+                  className="sign-box"
+                >
+                  <img src={weapon.img} alt={weapon.name + " for the game"} />
+                </div>
+              );
+            })}
+          </div>
+
+          <Info myChoice={this.state.myChoice} />
+        </main>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+function Header() {
   return (
-    <div className="sign-boxes">
-      <div id="rock" className="sign-box">
-        <img src={rock} alt="rock for the game" />
-      </div>
-      <div id="paper" className="sign-box">
-        <img src={paper} alt="paper for the game" />
-      </div>
-      <div id="scissors" className="sign-box">
-        <img
-          src={scissors}
-          alt="scissors for the game"
-        />
-      </div>
-    </div>
+    <header>
+      <h1>Rock Paper Scissors</h1>
+    </header>
   );
 }
-
-function Info() {
-  return (
-    <div>
-      <p id="info">Choose to start the game!</p>
-    </div>
-  );
-}
-
 function Footer() {
   return (
     <footer>
-      <p>
-        <a href="https://github.com/LD8" rel="noopener noreferrer" target="_blank">
+      <div>
+        <a
+          href="https://github.com/LD8"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           GitHub
         </a>
-      </p>
-      <p>
+      </div>
+      <div>
         <span>By </span>
-        <a href="https://DonLee.online" rel="noopener noreferrer" target="_blank">
+        <a
+          href="https://DonLee.online"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           Don Lee
         </a>
-      </p>
+      </div>
     </footer>
   );
 }
-
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>Rock Paper Scissors</h1>
-      </header>
-      <main>
-        <ScoreBoard />
-        <Status />
-        <SignBoxes />
-        <Info />
-      </main>
-      <Footer />
-    </div>
-  );
-}
-
 export default App;
